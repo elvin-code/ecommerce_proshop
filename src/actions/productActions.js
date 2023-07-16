4f37019e70163c37394454989e8bc64816f2ef07
Elvin Alvarez <elvinalvarez@Mac-mini.local>
2023-07-16 16:38:32 -0600

    ADD: init commit ecommerce Frontend

---
+++ src/actions/productActions.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import {
+    PRODUCT_LIST_REQUETS,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_REQUETS,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
+ } from '../constants/productConstants'
+
+export const listProducts = () => async (dispatch) => {
+    try{
+        dispatch({ type: PRODUCT_LIST_REQUETS })
+        const { data } = await axios.get('/api/products/')
+        dispatch({ 
+            type: PRODUCT_LIST_SUCCESS,
+            payload: data
+        })
+    }catch(error){
+        dispatch({ 
+            type: PRODUCT_LIST_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message
+        })
+    }
+}
+
+export const ListProductDetails = (id) => async (dispatch) => {
+    try{
+        dispatch({ type: PRODUCT_DETAILS_REQUETS })
+        const { data } = await axios.get(`/api/products/${id}/`)
+        dispatch({ 
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload: data
+        })
+    }catch(error){
+        dispatch({ 
+            type: PRODUCT_DETAILS_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message
+        })
+    }
+}
